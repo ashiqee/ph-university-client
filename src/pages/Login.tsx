e7030d6a8a05e423c22d3e6b7f6b0d1035210d20
ashiqee <ashiq.buet73@gmail.com>
2024-07-06 23:20:06 +0600

    login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,28 @@
+import { Button } from 'antd';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const {register,handleSubmit}= useForm();
+
+    const onSubmit = (data)=>{
+        console.log(data);
+        
+    }
     return (
         <div>
-            This Login Page
+            
+            <form  onSubmit={handleSubmit(onSubmit)}>
+        <div>
+            <label htmlFor="id">ID:</label>
+            <input type="text" id='id' {...register('id')} />
+        </div>
+        <div>
+            <label htmlFor="password">Password:</label>
+            <input type="text" id='password' {...register('password')} />
+        </div>
+        <Button htmlType='submit'>Login</Button>
+            </form>
         </div>
     );
 };
