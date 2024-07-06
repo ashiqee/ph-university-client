6bf11b0eb15331336a53d4c9d31c842b90081c8d
ashiqee <ashiq.buet73@gmail.com>
2024-07-06 13:18:09 +0600

    first coiit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            This Login Page
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/admin/CreateStudent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateStudent = () => {
+    return (
+        <div>
+            Create Studetn
+        </div>
+    );
+};
+
+export default CreateStudent;
