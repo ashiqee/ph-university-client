74c792a97a29302ac8920cf22c252a1e9f7b40cd
ashiqee <ashiq.buet73@gmail.com>
2024-07-08 00:06:41 +0600

    redux initial setup

---
+++ src/redux/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { baseApi } from './api/baseApi'
+
+export const store = configureStore({
+    reducer:{
+        [baseApi.reducerPath]: baseApi.reducer,
+
+    },
+
+    middleware:(getDefaltMiddleware)=>
+        getDefaltMiddleware().concat(baseApi.middleware),
+})
+
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
